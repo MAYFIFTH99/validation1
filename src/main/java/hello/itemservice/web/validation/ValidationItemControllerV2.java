b24aee40b2cd3735bdaa1f2e76ed8ae3ecc685a3
harvey <alstjr971@naver.com>
2024-12-29 12:32:48 +0900

    refactor : @InitBinder -> WebDataBinder로 validator 등록하고 사용하기
    @InitBinder(WebDataBinder dataBinder){
     dataBinder.addValidators(itemValidator)
    }
    @Validated @ModelAttribute Item item, ResultBinidng resultBinding..
    
    글로벌 설정은 전체 Application 클래스에 implements WebMvcConfigurer 를 구현하고,
    
    @Override
    public Validator getValidator(){
     return new ItemValidator();
    }
    를 사용하면 된다.

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -9,6 +9,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,6 +25,12 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    //컨트롤러가 호출될 때마다, WebDataBinder를 통해 Validator를 등록
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -172,7 +180,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add") // 오류 메시지를 errors.properties를 이용하여 국제화
+    //@PostMapping("/add") // 오류 메시지를 errors.properties를 이용하여 국제화
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         if (itemValidator.supports(Item.class)) {
             itemValidator.validate(item, bindingResult);
@@ -181,6 +189,20 @@ public class ValidationItemControllerV2 {
             if (bindingResult.hasErrors()) {
                 return "validation/v2/addForm";
             }
+
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add") // 오류 메시지를 errors.properties를 이용하여 국제화
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 실패 시, 입력 폼으로 다시 이동
+        if (bindingResult.hasErrors()) {
+            return "validation/v2/addForm";
+        }
 
 
         Item savedItem = itemRepository.save(item);
