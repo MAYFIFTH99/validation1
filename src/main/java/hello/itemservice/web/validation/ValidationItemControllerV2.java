1db141792e2ac98b46e3f881360f9a432ba03622
harvey <alstjr971@naver.com>
2024-12-29 12:14:25 +0900

    refactor: Validation 로직을 분리하기
    ItemValidator 클래스를 추가하고, @Component 애노테이션 적용 후
    Controller에서 주입 받아서 사용하기

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -22,6 +22,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -139,7 +140,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add") // 오류 메시지를 errors.properties를 이용하여 국제화
+    //@PostMapping("/add") // 오류 메시지를 errors.properties를 이용하여 국제화
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
@@ -164,6 +165,23 @@ public class ValidationItemControllerV2 {
         if(bindingResult.hasErrors()) {
             return "validation/v2/addForm";
         }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add") // 오류 메시지를 errors.properties를 이용하여 국제화
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if (itemValidator.supports(Item.class)) {
+            itemValidator.validate(item, bindingResult);
+        }
+            //검증 실패 시, 입력 폼으로 다시 이동
+            if (bindingResult.hasErrors()) {
+                return "validation/v2/addForm";
+            }
+
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
